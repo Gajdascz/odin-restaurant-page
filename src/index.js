4e124777d845d0bc1ace3b135d5c9f01be051d97
Nolan Gajdascz <78930672+Gajdascz@users.noreply.github.com>
2023-10-30 22:17:41 -0400

    Create basic HTML and style
    Renamed landing-page to restaurant-page for clarity
    Started structuring and styling the main index page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,2 @@
-import './components/landing-page/styles/mynormalize.css';
-import './components/landing-page/styles/style.css';
-
-function test() {
-  const headerElement = document.createElement('h1');
-  headerElement.textContent = `It's a test!`;
-  headerElement.classList.add('test');
-  return headerElement;
-}
-
-document.body.appendChild(test());
+import './components/restaurant-page/styles/mynormalize.css';
+import './components/restaurant-page/styles/style.css';
